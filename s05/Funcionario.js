ff3baa1f93568b0011868b7755509f908c718d7b
Bruno Ricardo Machado <bruno_r_machado@estudante.sesisenai.org.br>
2023-01-23 00:13:49 -0300

    semana 05 started

---
+++ s05/Funcionario.js
@@ -0,0 +1,19 @@
+class Funcionario {
+    cpf
+    nomeCompleto
+
+    salario
+
+    constructor(cpf, nomeCompleto, salario) {
+        this.cpf = cpf;
+        this.nomeCompleto = nomeCompleto;
+        this.salario = salario
+    }
+
+    promover(percentual) {
+        this.salario *= (1 + (percentual / 100))
+    }
+}
+
+
+export default Funcionario;
---
+++ s05/app.js
@@ -0,0 +1,8 @@
+import Funcionario from "./Funcionario.js";
+
+const bruno = new Funcionario('050.741.159-57', 'Bruno Ricardo Machado', 8000)
+
+console.log(bruno)
+bruno.promover(50)
+console.log(bruno)
+
